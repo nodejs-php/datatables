317492012bf4e34b84af1e9b1f5ded9ff8d69724
aleksey <av.pavlow@gmail.com>
2024-02-19 19:34:21 +0300

    Добавляет файлы

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,22 +4,30 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Yajra\DataTables\Html\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register() {
+            Event::listen('Illuminate\Database\Events\QueryExecuted', function ($query) {
+                // filter oauth ones
+                if (!str_contains($query->sql, 'oauth')) {
+                    Log::debug($query->sql . ' - ' . serialize($query->bindings));
+                }
+            });
     }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
         Builder::useVite();
+
     }
 }
